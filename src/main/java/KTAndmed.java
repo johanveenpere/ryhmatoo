f4dff67b5045865e4cb9721d7af1ccea803ae5cf
juhansa <juhansaaring@gmail.com>
2021-03-21 19:45:41 +0200

    tegin KokkuvõtteKoostaja, muutsin Andmed ja lisasin Andmed alamklassid RGAndmed ja KTAndmed

---
+++ src/main/java/KTAndmed.java
@@ -0,0 +1,28 @@
+public class KTAndmed extends Andmed{
+    private float compTomoDoseIndex; //Computed Tomography Dose Index
+    private float doseLengthProduct; // Dose-length Product
+
+    public KTAndmed(String viit, float kaal) {
+        super(viit, kaal);
+    }
+
+    public float getCTDIvol() {
+        return compTomoDoseIndex;
+    }
+
+    public float getDLP() {
+        return doseLengthProduct;
+    }
+
+    public void setCTDIvol(float cTDIvol) {
+        this.compTomoDoseIndex = cTDIvol;
+    }
+
+    public void setDLP(float dLP) {
+        this.doseLengthProduct = dLP;
+    }
+
+    public String toString(){
+        return super.toString() + ", " + compTomoDoseIndex + ", " + doseLengthProduct;
+    }
+}
