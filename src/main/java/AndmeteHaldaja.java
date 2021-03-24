17e9e15020534f4692f7c99aace951114df080c7
Tomi Kuusik <tomikuusik@gmail.com>
2021-03-25 01:49:59 +0200

    Lisa valimi sorteerimis funktsionaalsus, custom exception

--- src/main/java/AndmeteHaldaja.java
+++ src/main/java/AndmeteHaldaja.java
@@ -1,10 +1,13 @@
 import java.sql.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class AndmeteHaldaja {
+    private float alampiir;
+    private float ülempiir;
+    private float keskKaalKriiteerium = 75;
+    private float mootemääramatus = 2;
+    private int minValimSuurus = 0;
 
     // Ühenda andmebaasiga
     private Connection connect() {
@@ -12,21 +15,27 @@ public class AndmeteHaldaja {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(url);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return connection;
     }
-    // Initialiseeri andmebaas
-    public AndmeteHaldaja () {
+
+    // Initialiseeri andmebaas ja default parameetrid
+    public AndmeteHaldaja(int minValimSuurus, float keskKaalKriiteerium, float mootemääramatus, float alampiir, float ülempiir) {
+        this.keskKaalKriiteerium = keskKaalKriiteerium;
+        this.mootemääramatus = mootemääramatus;
+        this.minValimSuurus = minValimSuurus;
+        this.alampiir = alampiir;
+        this.ülempiir = ülempiir;
+
         try (Connection connection = this.connect()) {
             if (connection != null) {
                 DatabaseMetaData metadata = connection.getMetaData();
                 System.out.println("Andmebaasi driver on " + metadata.getDriverName());
                 System.out.println("Andmebaas edukalt loodud.");
 
-                String sqlCode = "CREATE TABLE IF NOT EXISTS patsient(" +
+                String sqlCode = "CREATE TABLE IF NOT EXISTS uuring(" +
                         "pildiviit varchar(16) NOT NULL PRIMARY KEY," +
                         "kaal FLOAT(24)," +
                         "sugu VARCHAR(1)," +
@@ -39,11 +48,11 @@ public class AndmeteHaldaja {
                     statement.execute(sqlCode);
                 }
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
+
     // Salvest andmebaasi andmeid
     public void salvestaAndmed(String pildiviit, Float kaal, String sugu, int vanus, Float doosiandmed, String idSeade) {
         String sqlCode = "INSERT INTO patsient VALUES(?,?,?,?,?,?,?)";
@@ -67,16 +76,15 @@ public class AndmeteHaldaja {
             }
 
             sqlStatement.executeUpdate();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-    // Küsi andmebaasist kõik andmed
-    public List<Andmed> getKoikPatsiendid() {
-        List<Andmed> koikPatsiendid = new ArrayList<>();
+
+    public List<Andmed> getUuringud() {
+        List<Andmed> koikUuringud = new ArrayList<>();
         try (Connection connection = this.connect()) {
-            String sqlCode = "SELECT pildiviit,kaal,sugu,vanus,doosiandmed from patsient;";
+            String sqlCode = "SELECT pildiviit,kaal,sugu,vanus,doosiandmed from uuring;";
             Statement sqlStatement = connection.createStatement();
             ResultSet resultSet = sqlStatement.executeQuery(sqlCode);
 
@@ -86,12 +94,57 @@ public class AndmeteHaldaja {
                 andmeObjekt.setKaal(resultSet.getFloat("kaal"));
                 andmeObjekt.setSugu(resultSet.getString("sugu"));
                 andmeObjekt.setDoosiandmed(resultSet.getFloat("doosiandmed"));
-                koikPatsiendid.add(andmeObjekt);
+                koikUuringud.add(andmeObjekt);
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return koikPatsiendid;
+        return koikUuringud;
+    }
+    public ArrayList<String> getValimPildiviidad() throws PuudulikValimException, SQLException {
+        ArrayList<Object[]> sorteeritudDataset = new ArrayList<>();
+        ArrayList<String> väljastatavValim = new ArrayList<>();
+        int valimSuurus = 0;
+
+        String sqlCode = "SELECT pildiviit,kaal, ABS(? - kaal) AS spread from uuring WHERE kaal < ? AND kaal > ? ORDER BY spread;";
+        try (Connection connection = this.connect()) {
+            PreparedStatement sqlStatement = connection.prepareStatement(sqlCode);
+
+            sqlStatement.setFloat(1,this.keskKaalKriiteerium);
+            sqlStatement.setFloat(2,this.ülempiir);
+            sqlStatement.setFloat(3,this.alampiir);
+            ResultSet resultSet = sqlStatement.executeQuery();
+
+            while (resultSet.next()) {
+                valimSuurus++;
+                String pildiviit = resultSet.getString("pildiviit");
+                float kaal = resultSet.getFloat("kaal");
+                sorteeritudDataset.add(new Object[]{pildiviit,kaal});
+            }
+
+            if (valimSuurus < minValimSuurus) {
+                throw new PuudulikValimException("Valim ei tule hindamisele. Kaalupiiridesse jäävaid patsiente on vähem kui valimi miinimum");
+            }
+
+            float kaalKokku = 0;
+            int uuringuidVäljastatamiseks = 0;
+
+            for (Object[] pildiviitKaal : sorteeritudDataset) {
+                väljastatavValim.add((String) pildiviitKaal[0]);
+                kaalKokku += (float) pildiviitKaal[1];
+                uuringuidVäljastatamiseks++;
+                float hetkeErinevusKeskmisest = Math.abs(this.keskKaalKriiteerium - kaalKokku / uuringuidVäljastatamiseks);
+                System.out.println(hetkeErinevusKeskmisest);
+                if (uuringuidVäljastatamiseks >= this.minValimSuurus &&  hetkeErinevusKeskmisest < mootemääramatus) {
+                    return väljastatavValim;
+                }
+            }
+            throw new PuudulikValimException("Valim ei tule kokku. Kaalupiiridesse jäävate patsientide kaalukeskmine on mõõteääramatusest väljas");
+        }
+
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
--- src/test/java/TestAndmebaas.java
+++ src/test/java/TestAndmebaas.java
@@ -3,12 +3,19 @@ import java.util.List;
 
 public class TestAndmebaas {
     public static void main(String[] args) {
-        AndmeteHaldaja haldaja = new AndmeteHaldaja();
-        //haldaja.salvestaAndmed("HTYKRG4210311xxx",Float.valueOf("23"), "M", 18, Float.valueOf("13"), "AF123");
+        AndmeteHaldaja haldaja = new AndmeteHaldaja(10, 80, 5, 60, 85);
+        try {
+            System.out.println(haldaja.getValimPildiviidad());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        /*
+        haldaja.salvestaAndmed("HTYKRG4210311xxx",Float.valueOf("23"), "M", 18, Float.valueOf("13"), "AF123");
         List<Andmed> koikPatisendid = haldaja.getKoikPatsiendid();
         for (Andmed patsient : koikPatisendid) {
             System.out.println(patsient.getViit());
             System.out.println(patsient.getKaal());
-        }
+         */
     }
 }
