2fd31ca700b6cadeaa7119457c271d8ab04b3652
juhansa <juhansaaring@gmail.com>
2021-04-02 20:50:55 +0300

    KujutiseFailiLugeja loomine, vastavatele Uuring alamklassidele laeAndmed meetodite lisamine

--- src/main/java/NimmelülidUuring.java
+++ src/main/java/NimmelülidUuring.java
@@ -1,17 +1,48 @@
-import com.pixelmed.dicom.AttributeList;
-import com.pixelmed.dicom.DicomFileUtilities;
-import com.pixelmed.dicom.DicomInputStream;
-import com.pixelmed.dicom.SetOfDicomFiles;
+import com.pixelmed.dicom.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
-public class NimmelülidUuring extends Uuring{
+public class NimmelülidUuring extends Uuring {
     private float doseAreaProductAP;
     private float doseAreaProductLAT;
     private float distanceSourceToPatientAP;
     private float distanceSourceToPatientLAT;
 
+    /**
+     * Meetod mis täidab isendiväljad väärtustega, pärides need etteantud failist KujutiseFailiLugeja klassi staatilise meetodi abil.
+     * Failist lugemise meetodile antakse edasi listidena failinimed ja DICOM atribuutide koodid, mida on vaja välja lugeda.
+     * Tagasi saadakse nimekiri, kus on iga faili kohta omakorda nimekiri atribuutidele vastavatest väärtustest.
+     * Isendiväljade täitmiseks on vaja teada, millisest failist pärinevaid andmeid parajasti loetakse,
+     * selle jaoks on loodud nn võtmeatribuut, mille abil neil vahet tehakse.
+     * @param failinimed - antud juhul on vaja lugeda andmed mitmest failist, seetõttu on sisendis List failinimedega
+     */
+    public void laeAndmed(List<String> failinimed) throws IOException, DicomException {
+        List<String> atribuutideNimekiri = new ArrayList<>(Arrays.asList(
+                "00185105", //vaade (AP või LAT) - võtmeatribuut
+                "00100040", //sugu
+                "00101010", //vanus
+                "0018115e", //doseAreaProduct
+                "00181110"  //distanceSourceToPatient
+        ));
+        List<List<String>> andmeteNimekirjad = KujutiseFailiLugeja.loeKujutiseFailist(failinimed, atribuutideNimekiri);
+        for (List<String> andmeteNimekiri : andmeteNimekirjad
+        ) {
+            switch (andmeteNimekiri.get(0)) {
+                case "AP" -> {
+                    setSugu(andmeteNimekiri.get(1));
+                    setVanus(Integer.parseInt(andmeteNimekiri.get(2).substring(0, 2))); //vanus on dicomis kujul nt "031Y"
+                    doseAreaProductAP = Float.parseFloat(andmeteNimekiri.get(3));
+                    distanceSourceToPatientAP = Float.parseFloat(andmeteNimekiri.get(4));
+                }
+                case "LAT" -> {
+                    doseAreaProductLAT = Float.parseFloat(andmeteNimekiri.get(3));
+                    distanceSourceToPatientLAT = Float.parseFloat(andmeteNimekiri.get(4));
+                }
+            }
+        }
+    }
 
     public NimmelülidUuring(String viit, float kaal) {
         super(viit, kaal);
@@ -49,8 +80,8 @@ public class NimmelülidUuring extends Uuring{
         this.distanceSourceToPatientLAT = distanceSourceToPatientLAT;
     }
 
-    public String toString(){
-        return super.toString() + ", " + doseAreaProductAP + ", " + distanceSourceToPatientAP+ ", " + doseAreaProductLAT + ", " + distanceSourceToPatientLAT;
+    public String toString() {
+        return super.toString() + ", " + doseAreaProductAP + ", " + distanceSourceToPatientAP + ", " + doseAreaProductLAT + ", " + distanceSourceToPatientLAT;
     }
 
 
--- src/main/java/RindkereUuring.java
+++ src/main/java/RindkereUuring.java
@@ -1,6 +1,33 @@
+import com.pixelmed.dicom.DicomException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RindkereUuring extends Uuring{
     private float doseAreaProduct;
     private float distanceSourceToPatient;
+
+    /**
+     * Meetod mis täidab isendiväljad väärtustega, pärides need etteantud failist KujutiseFailiLugeja klassi staatilise meetodi abil.
+     * Failist lugemise meetodile antakse edasi failinimi ja listina DICOM atribuutide koodid, mida on vaja välja lugeda.
+     * Tagasi saadakse  nimekiri atribuutidele vastavatest väärtustest.
+     * @param failinimi - antud juhul on vaja lugeda andmed mitmest failist, seetõttu on sisendis List failinimedega
+     */
+    public void laeAndmed(String failinimi) throws IOException, DicomException {
+        List<String> atribuutideNimekiri = new ArrayList<>(Arrays.asList(
+                "00100040", //sugu
+                "00101010", //vanus
+                "0018115e", //doseAreaProduct
+                "00181110"  //distanceSourceToPatient
+        ));
+        List<String> andmeteNimekiri = KujutiseFailiLugeja.loeKujutiseFailist(failinimi, atribuutideNimekiri);
+                setSugu(andmeteNimekiri.get(0));
+                setVanus(Integer.parseInt(andmeteNimekiri.get(1).substring(0,2))); //vanus on dicomis kujul nt "031Y"
+                doseAreaProduct = Float.parseFloat(andmeteNimekiri.get(2));
+                distanceSourceToPatient = Float.parseFloat(andmeteNimekiri.get(3));
+    }
 
     public RindkereUuring(String viit, float kaal) {
         super(viit, kaal);
