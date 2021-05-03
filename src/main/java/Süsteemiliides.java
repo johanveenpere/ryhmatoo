8475526756a0ca838ce4feaed31fbfa30269d052
Johan Veenpere <johan.veenpere@gmail.com>
2021-05-04 00:20:43 +0300

    WIP

--- src/main/java/Süsteemiliides.java
+++ src/main/java/Süsteemiliides.java
@@ -1,27 +1,61 @@
 import Model.PeaNatiivUuring;
+import Model.Uuring;
+import Repository.UuringRepository;
 import Service.Kriteerium;
 import Service.Valim;
 import Service.ValimiSelekteerija;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 
 public class Süsteemiliides {
-   public void SisestaUuring(String pildiviit, double kaal){
-        //salvestada andmebaasi pildiviit, kaal
+
+   private EntityManagerFactory emf;
+   private EntityManager em;
+   private UuringRepository db;
+   private Kriteerium kriteerium;
+
+   public Süsteemiliides(String andmebaasiNimi){
+      EntityManagerFactory emf = Persistence.createEntityManagerFactory(andmebaasiNimi);
+      em = emf.createEntityManager();
+      db = new UuringRepository(emf, em);
    }
 
-   public void TeeKokkuvõte(String failitee){
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("andmed");
-      OffsetDateTime test = OffsetDateTime.of(2020,1,1,1,1,1,1, ZoneOffset.UTC);
-      Kriteerium kriteerium = new Kriteerium(80,60,65,1,5, test);
-      ValimiSelekteerija selekteerija = new ValimiSelekteerija(PeaNatiivUuring.class, emf, kriteerium);
-      Valim valim = selekteerija.getValim();
+   public Süsteemiliides(EntityManagerFactory emf, EntityManager em){
+      db = new UuringRepository(emf, em);
+   }
+
+   public Süsteemiliides(UuringRepository db){
+      this.db = db;
+   }
+
+   public <T extends Uuring> void UusUuring(String pildiviit, double kaal, Class<T> tüüp) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+      //salvestada andmebaasi pildiviit, kaal
+      Constructor<T> con = tüüp.getConstructor(String.class, double.class);
+      T uuring = con.newInstance(pildiviit, kaal);
+      db.addUuring(uuring);
+   }
+
+   public void TäidaPuuduvadUuringud(){
+      //List<Uuring> uuringud = db.getPoolikudUuringud();
+
+   }
+
+   public void setKriteerium(Kriteerium kriteerium) {
+      this.kriteerium = kriteerium;
+   }
+
+   public void TeeKokkuvõte(String failitee, Class<? extends Uuring> tüüp){
+      List<Uuring> uuringud = db.getValimiKandidaadid(tüüp, kriteerium);
       try {
-         KokkuvõtteKoostaja.teeCSV(valim.getUuringud(), failitee);
+         KokkuvõtteKoostaja.teeCSV(uuringud, failitee);
       }
       catch(IOException e){
          e.printStackTrace();
