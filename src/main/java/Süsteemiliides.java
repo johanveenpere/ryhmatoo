ec59a40f5591b9ecc4956d03a5a19b608f8ee72a
Johan Veenpere <johan.veenpere@gmail.com>
2021-05-06 21:57:50 +0300

    Lõpetasin Süsteemiliideses poolikute uuringute uuendamise koodi ja parandasin UuringRepository konstruktorile antud argumendid. (git add --patch ei töötanud ja pidin ühe commitina tegema)

--- src/main/java/Süsteemiliides.java
+++ src/main/java/Süsteemiliides.java
@@ -1,71 +1,76 @@
-import Model.PeaNatiivUuring;
 import Model.Uuring;
 import Repository.UuringRepository;
 import Service.Kriteerium;
-import Service.Valim;
-import Service.ValimiSelekteerija;
+import com.pixelmed.dicom.DicomException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Süsteemiliides {
 
-   private EntityManagerFactory emf;
-   private EntityManager em;
-   private UuringRepository db;
-   private Kriteerium kriteerium;
+    private EntityManagerFactory emf;
+    private EntityManager em;
+    private UuringRepository db;
 
-   public Süsteemiliides(String andmebaasiNimi){
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory(andmebaasiNimi);
-      em = emf.createEntityManager();
-      db = new UuringRepository(emf, em);
-   }
+    public Süsteemiliides(String andmebaasiNimi) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(andmebaasiNimi);
+        em = emf.createEntityManager();
+        db = new UuringRepository(emf);
+    }
 
-   public Süsteemiliides(EntityManagerFactory emf, EntityManager em){
-      db = new UuringRepository(emf, em);
-   }
+    public Süsteemiliides(EntityManagerFactory emf, EntityManager em) {
+        db = new UuringRepository(emf);
+    }
 
-   public Süsteemiliides(UuringRepository db){
-      this.db = db;
-   }
+    public Süsteemiliides(UuringRepository db) {
+        this.db = db;
+    }
 
-   public <T extends Uuring> void UusUuring(String pildiviit, double kaal, Class<T> tüüp) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-      //salvestada andmebaasi pildiviit, kaal
-      Constructor<T> con = tüüp.getConstructor(String.class, double.class);
-      T uuring = con.newInstance(pildiviit, kaal);
-      db.addUuring(uuring);
-   }
+    public <T extends Uuring> void UusUuring(String pildiviit, double kaal, Class<T> tüüp) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        //salvestada andmebaasi pildiviit, kaal
+        Constructor<T> con = tüüp.getConstructor(String.class, double.class);
+        T uuring = con.newInstance(pildiviit, kaal);
+        db.addUuring(uuring);
+    }
 
-   public void TäidaPuuduvadUuringud(String tempPildikaust){
-      List<Uuring> uuringud = new ArrayList<>();//db.getPoolikudUuringud();
-      List<String> tõmmatudFailideTeed = new ArrayList<>();
-      CGetUuringud serveriühendus = new CGetUuringud();
-      for (Uuring uuring : uuringud) {
-         tõmmatudFailideTeed.add(serveriühendus.TõmbaUuringud(uuring.getViit(), tempPildikaust));
-      }
-   }
+    public void TäidaPuuduvadUuringud(String tempPildikaust) {
+        List<Uuring> uuringud = new ArrayList<>();//db.getPoolikudUuringud();
+        CGetUuringud serveriühendus = new CGetUuringud();
+        for (Uuring uuring : uuringud) {
+            String ülemkaust = serveriühendus.TõmbaUuringud(uuring.getViit(), tempPildikaust);
+            File path = new File(ülemkaust);
+            List<File> failid = Arrays.asList(path.listFiles());
+            try {
+                KujutiseFailiLugeja.loeKujutiseFailist(uuring, failid);
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (DicomException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-   public void setKriteerium(Kriteerium kriteerium) {
-      this.kriteerium = kriteerium;
-   }
+    public void teeKokkuvõte(String failitee, Class<? extends Uuring> tüüp, Kriteerium kriteerium) {
+        List<Uuring> uuringud = db.getValimiKandidaadid(tüüp, kriteerium);
+        try {
+            KokkuvõtteKoostaja.teeCSV(uuringud, failitee);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-   public void TeeKokkuvõte(String failitee, Class<? extends Uuring> tüüp){
-      List<Uuring> uuringud = db.getValimiKandidaadid(tüüp, kriteerium);
-      try {
-         KokkuvõtteKoostaja.teeCSV(uuringud, failitee);
-      }
-      catch(IOException e){
-         e.printStackTrace();
-      }
-   }
-
-   //private void
 }
