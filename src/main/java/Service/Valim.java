5bec5279b6e292e5d2e839a777a88bed15083248
Juhan <juhansaaring@gmail.com>
2021-05-13 18:35:48 +0300

    GUI arenduse käigus muudetus, lisatud väljad mida vaja GUI TableView jaoks

--- src/main/java/Service/Valim.java
+++ src/main/java/Service/Valim.java
@@ -1,23 +1,24 @@
 package Service;
 
 import Model.Uuring;
+import com.sun.istack.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Valim implements Comparable<Valim>{
+public class Valim implements Comparable<Valim> {
     private final Class<?> uuringType;
     private final Kriteerium kriteerium;
     private final List<Uuring> uuringud;
 
-    private String uuringunimetus;
     private double keskKaal = 0;
     private double hälve = 0;
     private int suurus = 0;
-    private int ootel = 0;
     private String staatus;
+    private String uuringunimetus;
+    private int ootel;
 
     private boolean vastabKriteeriumitele = false;
     private boolean inMõõtemääramatus = false;
@@ -30,42 +31,14 @@ public class Valim implements Comparable<Valim>{
         if (uuringud != null && uuringud.size() != 0) {
             Collections.sort(this.uuringud);
 
-            this.keskKaal = Math.round(uuringud.stream().mapToDouble(Uuring::getKaal).sum() / uuringud.size()*100) / (double) 100;
-            this.hälve = Math.round(Math.abs(kriteerium.getKeskKaal() - this.keskKaal)*100) / (double) 100;
+            this.keskKaal = Math.round(uuringud.stream().mapToDouble(Uuring::getKaal).sum() / uuringud.size() * 100) / (double) 100;
+            this.hälve = Math.round(Math.abs(kriteerium.getKeskKaal() - this.keskKaal) * 100) / (double) 100;
             this.suurus = uuringud.size();
 
             this.inMõõtemääramatus = this.hälve <= this.kriteerium.getMootemaaramatus();
             this.miinimumTäidetud = this.suurus >= this.kriteerium.getMinValim();
             this.vastabKriteeriumitele = this.inMõõtemääramatus && this.miinimumTäidetud;
         }
-    }
-
-    public Valim(List<Uuring> uuringud) {
-        this.kriteerium = uuringud.get(0).getKriteerium();
-        this.uuringType = uuringud.get(0).getClass();
-        this.uuringud = uuringud;
-        this.ootel = Math.toIntExact(uuringud.stream().filter(u -> !u.isAndmedlaetud()).count());
-        this.uuringunimetus = uuringud.get(0).getUuringunimetus();
-        if (uuringud != null && uuringud.size() != 0) {
-            Collections.sort(this.uuringud);
-
-            this.keskKaal = Math.round(uuringud.stream().mapToDouble(Uuring::getKaal).sum() / uuringud.size()*100) / (double) 100;
-            this.hälve = Math.round(Math.abs(kriteerium.getKeskKaal() - this.keskKaal)*100) / (double) 100;
-            this.suurus = uuringud.size();
-
-            this.inMõõtemääramatus = this.hälve <= this.kriteerium.getMootemaaramatus();
-            this.miinimumTäidetud = this.suurus >= this.kriteerium.getMinValim();
-            this.vastabKriteeriumitele = this.inMõõtemääramatus && this.miinimumTäidetud;
-        }
-        if (this.miinimumTäidetud) {
-            if (this.inMõõtemääramatus)
-                this.staatus = "Valim koos";
-            else
-                this.staatus = "Keskmine kaal ei sobi";
-        } else {
-            this.staatus = "Miinimum täitmata";
-        }
-
     }
 
     public Class<?> getUuringType() {
@@ -104,26 +77,37 @@ public class Valim implements Comparable<Valim>{
         return miinimumTäidetud;
     }
 
+    public void setUuringunimetus(String uuringunimetus) {
+        this.uuringunimetus = uuringunimetus;
+    }
+
     public String getUuringunimetus() {
         return uuringunimetus;
     }
 
+    public void setOotel(int ootel) {
+        this.ootel = ootel;
+    }
+
     public int getOotel() {
-        return ootel;
+        return  ootel;
+    }
+
+    public void setStaatus(String staatus) {
+        this.staatus = staatus;
     }
 
     public String getStaatus() {
-        return staatus;
+        return this.staatus;
     }
 
     @Override
     public String toString() {
-        String header = "Uuring=" + this.uuringType.getSimpleName() + ", " + kriteerium.toString() + ", uuringuid=" + this.suurus + ", keskmine=" + this.keskKaal + ", hälve=" + this.hälve + ", kaalunormis=" + this.inMõõtemääramatus +  ", miinimum täidetud=" + this.miinimumTäidetud + "\n";
+        String header = "Uuring=" + this.uuringType.getSimpleName() + ", " + kriteerium.toString() + ", uuringuid=" + this.suurus + ", keskmine=" + this.keskKaal + ", hälve=" + this.hälve + ", kaalunormis=" + this.inMõõtemääramatus + ", miinimum täidetud=" + this.miinimumTäidetud + "\n";
         String uuringud;
         if (this.uuringud != null) {
             uuringud = this.uuringud.stream().map(Uuring::simpleString).collect(Collectors.joining(", \n"));
-        }
-        else {
+        } else {
             uuringud = "";
         }
         return header + uuringud;
@@ -131,16 +115,16 @@ public class Valim implements Comparable<Valim>{
 
     @Override
     public int compareTo(Valim o) {
-        int miinimumTäidetud = Boolean.compare(this.miinimumTäidetud,o.miinimumTäidetud);
+        int miinimumTäidetud = Boolean.compare(this.miinimumTäidetud, o.miinimumTäidetud);
         if (miinimumTäidetud == 0) {
             if (!this.miinimumTäidetud) {
-                int suurus = Integer.compare(this.suurus,o.suurus);
+                int suurus = Integer.compare(this.suurus, o.suurus);
                 if (suurus != 0) {
                     return suurus;
                 }
             }
-            int kumbMõõtemääramatuses = Boolean.compare(this.inMõõtemääramatus,o.inMõõtemääramatus);
-            int kumbVäiksem =  Integer.compare(o.suurus,this.suurus);
+            int kumbMõõtemääramatuses = Boolean.compare(this.inMõõtemääramatus, o.inMõõtemääramatus);
+            int kumbVäiksem = Integer.compare(o.suurus, this.suurus);
             if (kumbMõõtemääramatuses == 0) {
                 if (this.inMõõtemääramatus && kumbVäiksem != 0) {
                     return kumbVäiksem;
